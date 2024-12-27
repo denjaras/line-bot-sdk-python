dbc12badbce6fc0938d82c1c0325ec5343444b98
denjaras <torikaikurato@gmail.com>
2024-12-27 16:40:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
+
+#以下から
+from linebot.v3.messaging import MessagingApi, Configuration
+from linebot.v3.webhooks import WebhookHandler
+
+configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
+line_bot_api = MessagingApi(configuration)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+import os 
+#ここまでkuratoの追記
+
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
 
 app = Flask(__name__)
 
@@ -28,5 +41,8 @@ def handle_message(event):
         TextSendMessage(text=f"あなたのメッセージ: {event.message.text}")
     )
 
+# 以下by kurato
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT", 5000))  # 環境変数PORTがない場合はデフォルトで5000を使用
+    app.run(host="0.0.0.0", port=port)  # ホストを0.0.0.0に設定して外部接続を許可
+
